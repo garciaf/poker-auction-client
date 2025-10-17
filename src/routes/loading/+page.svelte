17bf591e5903be9c51cfc47fdd2c7bc4c4de74c4
Fabien Garcia <>
2025-10-17 22:42:52 +0200

    [DESIGN] Loading screen

--- src/routes/loading/+page.svelte
+++ src/routes/loading/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="flex flex-col items-center h-full text-white p-8 space-y-8">
-  <div class="justify-center items-center flex h-full flex-col">
+  <div class="justify-center items-center flex h-full flex-col justify-center">
     <h1 class="text-5xl text-white uppercase mb-8">{ $loadingMessage.message }</h1>
   </div>
 </div>
