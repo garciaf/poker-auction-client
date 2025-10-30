b9f900d6cf79bef71f2d896b807a0643f0dcc099
Fabien Garcia <>
2025-10-30 21:22:59 +0100

    Fix build

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,11 @@
   import '../app.css';
   import LiveNavigation  from '$lib/liveNavigation';
   import PlayerTag from '$lib/components/PlayerTag.svelte';
+  import { base } from '$app/paths'; // ✅ use base path from SvelteKit
 </script>
 
 <svelte:head>
-  <link rel="icon" type="image/svg+xml" href="/images/favicon.svg" />
+  <link rel="icon" type="image/svg+xml" href="{base}/images/favicon.svg" />
 </svelte:head>
 
 <main class="bg-poker-green h-dvh text-white overflow-auto">
