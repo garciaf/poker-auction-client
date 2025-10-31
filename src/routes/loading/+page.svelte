4ea55b86b94d7d28eee7e1c7e71ad668f09991f9
Fabien Garcia <>
2025-10-31 07:44:20 +0100

    [FEATURE] Leave the holeCard in the loading page

--- src/routes/loading/+page.svelte
+++ src/routes/loading/+page.svelte
@@ -1,8 +1,10 @@
 <script>
     import { loadingMessage } from '$lib/stores/player';
+    import HoleCards from '$lib/components/HoleCards.svelte'
 </script>
 
 <div class="flex flex-col items-center h-full text-white p-8 space-y-8">
+  <HoleCards />
   <div class="justify-center items-center flex h-full flex-col justify-center">
     <h1 class="text-5xl text-white uppercase mb-8">{ $loadingMessage.message }</h1>
   </div>
