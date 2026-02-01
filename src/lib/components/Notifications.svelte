99c505300b2e243b19f1eb9bb11b9320e5d58cd9
Fabien Garcia <>
2026-02-01 09:06:35 +0100

    [FEATURE] Allow notification from any user

--- src/lib/components/Notifications.svelte
+++ src/lib/components/Notifications.svelte
@@ -9,17 +9,15 @@
 <div aria-live="assertive" class="pointer-events-none fixed inset-0 flex items-start justify-end p-6 pt-20 z-[9999]">
     <div class="flex w-full max-w-sm flex-col items-end space-y-4">
         {#each $notifications as notification (notification)}
-            <div 
-                class="pointer-events-auto w-full overflow-hidden rounded-lg shadow-lg ring-1 ring-black/5"
+            <div
+                class="pointer-events-auto w-full overflow-hidden shadow-lg ring-1 ring-black/5"
                 in:fly="{{ x: 400, duration: 500, opacity: 0 }}"
                 out:fade="{{ duration: 300 }}"
-            > 
-                <div class="flex items-center space-x-4 bg-gray-900 rounded py-2 px-4">
-                    <div class="ml-3 w-0 flex-1 text-white font-[420]">
-                        {notification.message}
-                    </div>
-                    <div class="shrink-0 pt-0.5">
-                        <img class="size-10 rounded-full" src={`${base}/images/auctionner.png`} alt="auctioneer">
+            >
+                <div class="flex items-center space-x-4 bg-poker-darker-green border-2 border-poker-green rounded py-2 px-4">
+                    <div class="ml-3 w-0 flex-1 text-white font-eight-bit">
+                        <p class="text-gold font-semibold text-sm truncate max-w-[200px]">{notification.author}</p>
+                        <p>{notification.message}</p>
                     </div>
                 </div>
             </div>
